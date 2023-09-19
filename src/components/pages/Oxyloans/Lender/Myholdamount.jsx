d1e837ccde71b151a24d215ea5fae67137fa22a2
livinmandeva <livinmandeva@gmail.com>
2023-09-19 23:58:01 +0530

    updates on pagination

--- src/components/pages/Oxyloans/Lender/Myholdamount.jsx
+++ src/components/pages/Oxyloans/Lender/Myholdamount.jsx
@@ -112,6 +112,7 @@ const Myholdamount = () => {
                           total: datasource.length,
                           showTotal: (total, range) =>
                             `Showing ${range[0]} to ${range[1]} of ${total} entries`,
+                          position: ["topRight"],
                         }}
                         columns={columns}
                         dataSource={datasource}
