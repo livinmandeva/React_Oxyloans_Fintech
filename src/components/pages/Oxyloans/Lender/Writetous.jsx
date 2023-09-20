194de6238fb8ea62d1035b00470f0d5ee0486d1c
anjanyelle <anjanyelle@gmail.com>
2023-09-20 18:25:10 +0530

    upload

--- src/components/pages/Oxyloans/Lender/Writetous.jsx
+++ src/components/pages/Oxyloans/Lender/Writetous.jsx
@@ -1,16 +1,86 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import FeatherIcon from "feather-icons-react";
 import { pagination, Table } from "antd";
 import { onShowSizeChange, itemRender } from "../../../Pagination";
 import Header from "../../../Header/Header";
 import SideBar from "../../../SideBar/SideBar";
+import { getUserDetails ,writequery ,fileuploads} from "../../../HttpRequest/afterlogin";
 import Footer from "../../../Footer/Footer";
 
 import { allqueries, cancelled, resolved, pending } from "../../../imagepath";
+import { HandleClick } from "../../Base UI Elements/SweetAlert";
+import { Upload } from "feather-icons-react/build/IconComponents";
 
 const Writetous = () => {
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const [userdata,setuserdata] =useState({
+    profileData:{
+}})
+ const [files,setfile]=useState(null)  
+const [queryfiledinput,setqueryfiledinput]=useState("")
+
+
+  useEffect(() => {
+    getUserDetails().then((data) => {
+      setuserdata({
+        ...userdata,
+        profileData: data,
+      });
+    });
+
+
+    // alert(userdata)
+  
+  }, []);
+
+
+console.log(userdata)
+
+const handleWriteClick= async ()=>{
+  handleimageupload()
+
+  const response= writequery( userdata, queryfiledinput)
+  response.then((data)=>{
+    console.log(data);
+    if (data.request.status == 200) {
+         alert(data.status)
+    } else {
+     alert("error")
+    }
+  })
+
+}    
+const handleChange=(event)=>{
+
+  const {  name,value}= event.target;
+  setqueryfiledinput({
+    ...queryfiledinput,
+     [name]:value
+  })
+}
+
+const imageSubmit= (event)=>{
+  const file=event.target.files[0];
+  setfile(file)
+}
+
+const handleimageupload =async ()=>{
+           
+  const response= fileuploads(files)
+
+  console.log("file",files)
+  response.then((data)=>{
+    console.log(data);
+    if (data.request.status == 200) {
+         alert("success")
+    } else {
+     alert("error")
+    }
+  })
+
+}
+
 
   return (
     <>
@@ -132,7 +202,9 @@ const Writetous = () => {
                             Write A query
                             <span className="login-danger">*</span>
                           </label>
-                          <textarea className="form-control"></textarea>
+                          <textarea className="form-control" 
+                          
+                          name="queryfiledinput" onChange={handleChange} ></textarea>
                           {/* <input type="textarea" className="form-control" /> */}
                         </div>
                       </div>
@@ -140,16 +212,17 @@ const Writetous = () => {
                         <div className="form-group local-forms pull-right">
                           <div className="form-group service-upload">
                             <span>Upload Image</span>
-                            <input type="file" multiple="" />
+                            <input type="file" multiple=""  onChange={imageSubmit} />
                           </div>
 
                           {/* <input type="text" className="form-control" /> */}
                         </div>
                       </div>
                     </div>
+                    {/* <p>Email: {userdata.profileData.data.email}</p>     <p>mobileNumber: {userdata.profileData.data.mobileNumber}</p> */}
 
                     <div className="row col-12">
-                      <button className="btn btn-primary col-md-3 mx-3">
+                      <button className="btn btn-primary col-md-3 mx-3" onClick={handleWriteClick}>
                         Submit
                       </button>
                     </div>
