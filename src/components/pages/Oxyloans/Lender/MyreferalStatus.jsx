4de9158b9b6cb621be7ced4c65410434a7426327
anjanyelle <anjanyelle@gmail.com>
2023-09-26 17:05:28 +0530

    mes

--- src/components/pages/Oxyloans/Lender/MyreferalStatus.jsx
+++ src/components/pages/Oxyloans/Lender/MyreferalStatus.jsx
@@ -128,7 +128,7 @@ const downloadreferalstatus=async()=>{
                   <h3 className="page-title">Referral Infos</h3>
                   <ul className="breadcrumb">
                     <li className="breadcrumb-item">
-                      <Link to="/admindashboard">Dashboard</Link>
+                      <Link to="/admindashboard">Dashboard </Link>
                     </li>
                     <li className="breadcrumb-item active">My Deals</li>
                   </ul>
