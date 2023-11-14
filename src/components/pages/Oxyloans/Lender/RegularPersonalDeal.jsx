90208262b9f7de3e7592c78cf3efc75326b346c9
anjanyelle <anjanyelle@gmail.com>
2023-11-14 12:36:56 +0530

    change

--- src/components/pages/Oxyloans/Lender/RegularPersonalDeal.jsx
+++ src/components/pages/Oxyloans/Lender/RegularPersonalDeal.jsx
@@ -87,7 +87,8 @@ const RegularPersonalDeal = () => {
     };
 
     handleRegular();
-  }, [personal_runningDeal.pageno]);
+  }, []);
+
   return (
     <div className="main-wrapper">
       {/* Header */}
@@ -137,8 +138,7 @@ const RegularPersonalDeal = () => {
           </div>
           {personal_runningDeal.apidata.listOfDealsInformationToLender && (
             <>
-              {personal_runningDeal.apidata.listOfDealsInformationToLender !==
-              ""
+              {personal_runningDeal.apidata.listOfDealsInformationToLender !== ""
                 ? personal_runningDeal.apidata.listOfDealsInformationToLender.map(
                     (data, index) => (
                       <div className="row" key={index}>
@@ -260,23 +260,131 @@ const RegularPersonalDeal = () => {
                 : null}
             </>
           )}
+        {personal_runningDeal.apidata.listOfBorrowersDealsResponseDto && (
+          <>
+          {personal_runningDeal.apidata.listOfBorrowersDealsResponseDto !== ""
+                  ? personal_runningDeal.apidata.listOfBorrowersDealsResponseDto.map(
+                      (data, index) => (
+                        <div className="row" key={index}>
+                          <div className="col-sm-12 col-lg-12 col-xl-12 col-12 my-lg-2">
+                            <div className="card invoices-grid-card w-100 h-25">
+                              <div className="card-header row">
+                                <Link
+                                  to="/viewinvoice"
+                                  className="invoice-grid-link col-sm-12 col-lg-4"
+                                >
+                                  Deal Name: {data.dealName}
+                                </Link>
 
-          {personal_runningDeal.apidata.listOfBorrowersDealsResponseDto && (
-            <>
-              <div className="card">
-                <Table
-                  columns={columns}
-                  dataSource={
-                    personal_runningDeal.apidata.listOfBorrowersDealsResponseDto
-                      .length !== 0
-                      ? dataSource
-                      : []
-                  }
-                  pagination={false}
-                />
-              </div>
-            </>
-          )}
+                                <div className="col-sm-12 col-lg-2">
+                                  RoI : {data.rateOfInterest} %
+                                </div>
+                                <div className="col-sm-12 col-lg-3">
+                                  Tenure : {data.duration} Months
+                                </div>
+                                <div className="col-auto col-lg-3">
+                                  Deal Value : {data.dealAmount}
+                                </div>
+                                {/* <div>Status : Open</div> */}
+                              </div>
+                              <div className="card-middle row">
+                                <div className="col-sm-12 col-lg-6">
+                                  <h6>
+                                    Deal Opened Time :{" "}
+                                    {data.fundsAcceptanceStartDate}
+                                  </h6>
+                                  <h6>
+                                    Deal Closing Time :{" "}
+                                    {data.fundsAcceptanceEndDate}
+                                  </h6>
+                                  <h6>
+                                    First Participation :{" "}
+                                    {data.firstParticipationDate}
+                                  </h6>
+                                  <h6>
+                                    Last Participation :{" "}
+                                    {data.lastParticipationDate}
+                                  </h6>
+                                </div>
+                                <div className="col-sm-12 col-lg-6">
+                                  <small>
+                                    Comments : {data.messageSentToLenders}
+                                  </small>
+                                </div>
+                              </div>
+                              <div className="card-body">
+                                <div className="row align-items-center">
+                                  <div className="col-sm-6 col-lg-2">
+                                    <span>Available Limit</span>
+                                    <h6 className="mb-0">
+                                      {data.remainingAmountToPaticipateInDeal}
+                                    </h6>
+                                  </div>
+                                  <div className="col-sm-6 col-lg-2">
+                                    <span>Min Amount</span>
+                                    <h6 className="mb-0">
+                                      {data.minimumAmountInDeal}
+                                    </h6>
+                                  </div>
+                                  <div className="col-sm-6 col-lg-2">
+                                    <span>Max Amount</span>
+                                    <h6 className="mb-0">
+                                      {data.lenderPaticipationLimit}
+                                    </h6>
+                                  </div>
+                                  <div className="col-sm-6 col-lg-2">
+                                    <span>ATW</span>
+                                    <h6 className="mb-0">
+                                      {data.withdrawStatus}
+                                    </h6>
+                                  </div>
+
+                                  {data.withdrawStatus == "YES" && (
+                                    <div className="col-sm-6 col-lg-2">
+                                      <span>ATW ROI</span>
+                                      <h6 className="mb-0">
+                                        {data.roiForWithdraw} %
+                                      </h6>
+                                    </div>
+                                  )}
+
+                                  <div className="col-sm-6 col-lg-3">
+                                    <span>Deal Status</span>
+                                    <h6 className="mb-0">
+                                      {data.fundingStatus}
+                                    </h6>
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="card-footer">
+                                <div className="row align-items-center">
+                                  <div className="col-auto">
+                                    <Link
+                                      to={`participatedeal?dealId=${data.dealId}`}
+                                      className="badge bg-success-dark"
+                                    >
+                                      Participate
+                                    </Link>
+                                  </div>
+
+                                  <div className="col-auto">
+                                    <a
+                                      href={data.dealLink}
+                                      className="badge bg-primary-dark"
+                                      target="_blank"
+                                    >
+                                      View Borrower Documents
+                                    </a>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      )
+                    )
+                  : null} 
+      </> )}
         </div>
       </div>
       {/* /Page Wrapper */}
